f09f2af234777ced60536735010f5853de130e5e
Nayara <n.carvalhosilvabr@gmail.com>
2022-10-07 17:33:21 -0300

    first commit

---
+++ lojadegames/src/main/java/com/generation/lojadegames/model/Produto.java
@@ -0,0 +1,87 @@
+package com.generation.lojadegames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "Preenchimento obrigatório")
+	@Size(min = 2, max = 100, message = "O nome deve conter no mínimo 2 e no máximo 100 caracteres")
+	private String nome;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING)
+    @NotNull(message = "Preço é obrigatório!")
+    @Positive(message = "O preço deve ser maior do que zero!")
+    private BigDecimal preco;
+	
+	@NotNull
+	private Long estoque;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Long getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Long estoque) {
+		this.estoque = estoque;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+
+}
